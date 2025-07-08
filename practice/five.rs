8030d211ceedaa1bbaf8eb53b3172d02c13de989
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-07-08 21:13:11 +0530

    practice

--- practice/five.rs
+++ practice/five.rs
@@ -1,18 +1,24 @@
-fn main() {
-    let i = 6;
-    
-    call_int(i);
-    println!("after calling the func value od i : {}",i);
+fn main () {
+    let s1 = give_ownership();
+    println!("s1 :{}",s1);
 
-    let s = String::from("hello");
-    call_string(s);
-    println!("after calling fun the value of s :{}",s);
+    let s2 = String::from("Hello from main");
+    let s3 = take_and_give_ownership(s2);
+    println!("s3 : {}",s3);
 }
 
-fn call_int(i:32){
-    println!("call_int i: {}",i);
+// func to give owenrship of a string to another function 
+fn give_ownership -> String {
+    let some_string = String::from("Hello from give_ownership",some_string);
 }
 
-fn call_string(s:String) {
-    println!("call_string s: {}",s);
+// function to take and return ownership of a string
+
+fn take_and_give_ownership(some_string : String) -> String {
+    some_string
+}
+
+fn caluclate_length (s:String) -> (String,usize){
+    let length = s.len();
+    (s,length)
 }
