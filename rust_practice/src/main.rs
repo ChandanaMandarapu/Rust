f74f01c6fad26262dde8e866f56f8791f1aeec02
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-07-09 21:32:00 +0530

    some brainstorming

--- rust_practice/src/main.rs
+++ rust_practice/src/main.rs
@@ -5,14 +5,11 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("What is your name?");
-    
-    let mut name = String::new();
-    let greeting = String::from("Nice to meet u");
-
-    io::stdin()
-        .read_line(&mut name)
-        .expect("Didn't receive input");
-
-    println!("Hello {} {}", name.trim_end(), greeting);
+    const ONE_MIL : u32 = 1_00_000;
+    const PI : f32 = 3.1415;
+    let age: &str = "48";
+    let mut age : u32 = age.trim().parse()
+    .expect(msg:"age wasnt assigned a number");
+age = age + 1;
+println!("Im {} and I want ${}",age,ONE_MIL);
 }
