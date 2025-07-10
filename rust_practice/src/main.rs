98fdb6e3e5c66539f7f3b32b02bb53aa345fb9ee
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-07-10 17:39:20 +0530

    lucky number game

--- rust_practice/src/main.rs
+++ rust_practice/src/main.rs
@@ -1,15 +1,13 @@
 use rand::Rng;
 
 fn play_around() {
-
     let mut rng = rand::thread_rng();
-    let num = rng.gen_range(1..=6); //generates number from 1 to 6
 
     let player_roll = rng.gen_range(1..=6);
     let bot_roll = rng.gen_range(1..=6);
 
-    println!("You rolled : {}",player_roll);
-    println!("Bot rolled : {}",bot_roll);
+    println!("You rolled : {}", player_roll);
+    println!("Bot rolled : {}", bot_roll);
 
     if player_roll > bot_roll {
         println!("you win this round");
@@ -19,8 +17,25 @@ fn play_around() {
         println!("its a draw");
     }
 
-}
-fn main () {
-    play_around();
+    let lucky_number = 4;
+    let mut won = false;
+
+    for _ in 1..=9 {
+        let roll = rng.gen_range(1..=6);
+        println!("bot rolled : {}", roll);
+
+        if roll == lucky_number {
+            println!("lucky roll you win");
+            won = true;
+            break;
+        }
+    }
+
+    if !won {
+        println!("Better luck next time!");
+    }
 }
 
+fn main() {
+    play_around();
+}
