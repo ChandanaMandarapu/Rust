63763c68bbe94a5dbc73a9c23acb01f97a73061f
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-10-13 13:34:13 +0530

    annotations in structs and static lifetime

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,107 +1,74 @@
+// life time annotations in structs
+// here the struct holds a refernce to a string slice and that reference has liftime of 'a 
+struct ImportantExcerpt <'a>{part: &'a str,}
 
-    // most imp topic of rust - lifetimes
-    // life time definition lies in the name itself a lifetime how long a pieve of data is valid for every reference in rust has a lifetime which is the scope for which reference is valid lifetime is not about how long a value lives they're about how long a REFERENCE to that value is valid
+fn main() {
+    let book = String::from("srimad. bhagvatam"),
+    let first_sentence = book.split('.').next().expect("could not find '.'");
+    let excerpt = ImportantExcerpt {
+        part: first_sentence,
+    };
+    println!("Excerpt : {}",excerpt.part);
+}
+// here book lives for entire main fun so the refernce first_Sentence is valdie and also the excerpt is valid 
 
-    /* fn main() {
-    let r;                // ---------+-- 'a
-                          //          |
-    {                     //          |
-        let x = 5;        // -+-- 'b  |
-        r = &x;           //  |       |
-    }                     // -+       |
-                          //          |
-    println!("r: {}", r); //          |
-}                         // ---------+ */
-    // lifetime annotations (syntax explained)
+// heres a thing which will not work see
 
-    // this wont compile the error will say something x does nor live long enough so whats happening here 
-    // we declared r but didnt initialise it yet
-    // we enter a new scope {}
-    // inside the scope we create x with value 6
-    // we make r point to x 
-    // scope ends and x is destroyed then nd their itself
-    // we try to use r but r points to dead memory which is like a dangling pointer in cpp 
-
-    // when rust cant figure out lifetimes automatically u have to annotate them 
-    // whats happening
-    // <'a> - declares a lifetime parameter named 'a 
-    // x :&'a str - x is a refernce to string slice that lives for lifetime 'a
-    // y : &'a str - y is a refernce to string slice that lives for same lifetime 'a
-    // -> &'astr - return value is refernce that lives for lifetime 'a again
-
-    // chandu chandu listen its easy - in simple words this below function takes two string slices that both live for same lifetime 'a and returns a string slice that lives for that same lifetime 'a as simple as that
-
-
-    fn longest<'a>(x: &'a str, y: &'a str) -> &'a str{
-        if x.len() > y.len() {
-            x 
-        } else {
-            y
-        }
-    }
-
-
-    fn main () {
-        let string1 = String::from("long string is long"); 
-        let string2 = String::from("xyz");
-
-        let result = lonest(string1.as_str(),string2.as_str());
-        println!("longest string is {}",result);
-    }
-
-
-    // what if lifetimes are diff
-    // string2 dies at teh end of innerscope but here we are asking result as result might have a refernce to string 2 rust wont at all allow it throws error 
-    /*fn main() {
-    let string1 = String::from("long string is long");
-    let result;
+/*fn main() {
+    let excerpt;
     
     {
-        let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
+        let novel = String::from("srimad . bhagvadam");
+        let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+        
+        excerpt = ImportantExcerpt {
+            part: first_sentence,
+    };
     }
     
-    println!("The longest string is {}", result);  // ERROR!
-}*/
+    println!("Excerpt: {}", excerpt.part);  // ERROR!
+} */
+// Why? Because novel dies at the end of the inner scope, which means first_sentence (which is a reference to part of novel) becomes invalid, 
 
-// life time ellision rules - when u dont neeed to annotations
+// which means excerpt.part is now a dangling reference like a dangling pointer in spp . Rust catches this at compile time.
 
-// rule 1 - each refernce parameter gets its own lifetime parameter 
+// The struct lifetime parameter 'a means: "This struct is only valid for as long as the data it references is valid."
 
-fn fool(x:&str) -> &str which is automaticaly fn fool<'a>(x:&'astr) -> &str
+// things getting complex here
 
-// rule2 - if theres exactly one input lifetime parameter that lifetime is assigned to all output lifetime parameters what does that mean
-
-fn foo(x:&str) -> &str which becomess fn foo<'a>(x:&'a str) -> &'a str = see the subtle difference lifetime of a parameter will be assigned to al outcomes 
-
-// rule 3 - if there are multiple input lifetime parameters but one of them is &self or &mut self = because its a method lifetime of self is assigned to all output lifetime parameters
-
-impl MyStruct{
-    fn get_data(&self) -> &str{
-        &self.data
-    }
+// multiple lifetime parameters
+// first and second can have diff lifetimes 
+struct Container <'a,'b>{
+    first: &'a str,
+    second: &'b str,
 }
-// becomes 
-
-impl MyStuct{
-    fn get_data<'a>(&'a self) -> &'a str{
-        &self.data
-    }
-}
-
-// lifetime annotations in structs
-
-struct ImportantExcerpt<'a> {
-    part: &'a str,
-}
-this means struct holds a refernce to a string slice and that refernce has lifetime 'a the struct cannot outlive the data its referencing 
+// using above lifetime scenario here
 
 fn main () {
-    let book = String::from(" SriMad Bhagvadgita..");
-    let first_sentence = book.split('.').next().expect("could not find a '.'");
+    let long_lived = String::from("I live long");
 
-    let excerpt = ImportantExcerpt{
-        part:first_sentence,
-    };
-    println!("Excerpt {}",excerpt.part);
+    {
+        let short_lived = String::from("I die soon");
+
+        let container = Container {
+            first: &long_lived,
+            second: &short_lived,
+        };
+
+        println!("{} and {}",container.first,container.last);
+    }
+
+    // long lived is still accesiblr here but not short lived as said it ends in that scope itseslf
+
+    // the 'static lifetime - wahh this is my fav this lives for whole entire program i was thinking what if smtng needs to live until very end 
+
+    // just got to know strin literals are static because they are within in ur programs binary they exist before main runs and after main() ends
+
+    // let s : &'static str = "lives forever";
+
+    static GREETING: &str = "Hello, world!";  // A global constant
+
+    fn get_greeting() -> &'static str {
+    GREETING
+    }
 }
