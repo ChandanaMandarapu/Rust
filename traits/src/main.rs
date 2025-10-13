d6f5c199531e2de88bec31b7872d71acc4a9b0e0
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-10-13 05:39:05 +0530

    traits - derives macros

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,65 +1,124 @@
-// traits are like interfaces or abstract classes in cpp in simple words rust actually needs a way to share behaviour without sharing data structure so traits help u align with that method without focing them to be the same type of thing. 
+// defining traits
+// derive macros automatic trait
+// here #derive() this tells rust please automatically implement these traits for me 
 
-    // &self - this meethod needs to look ar data inside the thing thats their but it wont changee it 
+#[derive(Debug, Clone, PartialEq)]
+struct Dummy; // derive can only be used on structs/enums, not traits
 
-    // implementing a trait for a Type 
-
-    trait Swim {
-    fn swim(&self);
-    } 
-    trait MakeNoise {
-        fn make_noise(&self);
-    }
-    struct Dog{
-        name:String,
-        weight: u32,
+trait Swim {
+    fn swim(&self) {
+        println!("swimming in a generic way...");
     }
 
-    struct Fish{
-        species:String,
-        depth_preference: u32,
-    }
-    impl Swim for Dog{
-        fn swim(&self){
-            println!("{} is doing the paddle",self.name);
-            if self.weight > 50 {
-                println!("its a bit slower",self.name);
-            }
+    // default behaviour
+    fn swim_distance(&self, distance: u32) {
+        for _ in 0..distance {
+            self.swim();
         }
     }
+}
 
-    impl Swim for Fish{
-        fn swim(&self){
-            println!("the {} is swimming through water at {} meters depth",
-            self.species, self.depth_preference);
+trait MakeNoise {
+    fn make_noise(&self);
+}
+
+struct Dog {
+    name: String,
+    weight: u32,
+}
+
+struct Fish {
+    species: String,
+    depth_pref: u32,
+}
+
+// using default feature
+struct Duck {
+    name: String,
+}
+
+// implementing for dog 
+impl Swim for Dog {
+    fn swim(&self) {
+        println!("{} is doing the doggy", self.name);
+        if self.weight > 50 {
+            println!("its a bit slower because {} is a big dong", self.name);
         }
     }
+}
 
-    impl MakeNoise for Dog{
-        fn make_noise(&self) {
-            println!("bow bow"); //wwwaaat the ff  haha
-        }
+// implementing for a fish
+impl Swim for Fish {
+    fn swim(&self) {
+        println!(
+            "the {} is gliding through water at {} meters depth",
+            self.species, self.depth_pref
+        );
     }
+}
 
-    fn make_it_swim(creature:&impl Swim){
-        creature.swim();
+impl Swim for Duck {
+    fn swim(&self) {
+        println!("{} is paddling with feet", self.name);
     }
+}
 
-    fn swim_and_bark(animal : &(impl Swim + MakeNoise)){
-        animal.swim();
-        animal.make_noise();
+impl MakeNoise for Dog {
+    fn make_noise(&self) {
+        println!("woooo");
     }
+}
+
+// using polymorphism this make_it_swim function can work with many other diff things its just a refeernce away behind the scenees rust actually creates two versions of make_it_swim dog and one for fish this is called monomorphization rust generates specialised code for each type at compile time 
+fn make_it_swim(creature: &impl Swim) {
+    creature.swim();
+}
+
+// here  + symbol combines trait requirements. The impl Swim + MakeNoise means "I need something that can do BOTH swimming AND making noise
+fn swim_and_bark(animal: &(impl Swim + MakeNoise)) {
+    animal.swim();
+    animal.make_noise();
+}
+
+// in real case scenarios
+// The <T: Swim + MakeNoise> is called a "generic with trait bounds
+// fn swim_and_bark<T: Swim + MakeNoise>(animal: &T){
+//     animal.swim();
+//     animal.make_noise();
+// }
+
 fn main() {
-   let buddy = Dog{
-    name: String::from("Buddy"),
-    weight:65,
-   };
+    let buddy = Dog {
+        name: String::from("Buddy"),
+        weight: 70,
+    };
+    let nemo = Fish {
+        species: String::from("goldfish"),
+        depth_pref: 10,
+    };
+    let dina = Duck {
+        name: String::from("dina"),
+    };
 
-   let gold = Fish{
-    species:String::from("goldfish");
-    depth_preference: 10,
-   };
+    // just testing the debug clone and partialeq traits
+    // this lets you print the struct for debugging purposes using {:?}:
+    // let buddy = Dog{name:String::from("BUddy"),age:5};
+    // println!("{:?}",buddy);
 
-   make_it_swim(&buddy);
-   make_it_swim(&nemo);
+    // clone trait
+    // let buddy = Dog{name:String::from("BUddy"),age:5};
+    // let buddy_clone = buddy.clone();
+
+    // PartialEq trait - lets us compare like comparing 2 dogs
+
+//     let buddy1 = Dog{name:String::from("BUddy"),age:5};
+//     let buddy2 = Dog{name:String::from("BUddy"),age:5};
+
+//     if buddy1 == buddy2 {
+//     println!("These are the same dog!");
+// }
+
+    make_it_swim(&buddy);
+    make_it_swim(&nemo);
+    dina.swim_distance(3);
 }
