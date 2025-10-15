9e43d9e944ec770a35e162fd10b2e9a53e351234
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-10-16 00:21:13 +0530

    umm just checkign my memory of borrowing and ownership pracice sessions

--- practice/six.rs
+++ practice/six.rs
@@ -1,5 +1,27 @@
-// slices in rust are a reference to contigous sequence of elements in a collection etc etc
+fn print_ref(s: &String) {
+    println!("{}", s);
+}
+fn main() {
+    let name = String::from("Rust");
+    print_ref(&name);
+}
 
-fn main () {
-    let arr : [char;5] = ['a','b','c','d','e'];
+fn add_text(s: &mut String) {
+    s.push_str(" World");
+}
+fn main() {
+    let mut name = String::from("Hello");
+    add_text(&mut name);
+    println!("{}", name);
+}
+
+
+fn take(s: String) -> String {
+    println!("{}", s);
+    s
+}
+fn main() {
+    let name = String::from("Rust");
+    let name = take(name);
+    println!("{}", name);
 }
